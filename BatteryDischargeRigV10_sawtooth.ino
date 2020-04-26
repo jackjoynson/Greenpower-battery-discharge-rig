58fee04421dcfb4d3cccfbddf2be27d11c80274a
Jack Joynson <jackjoynson97@gmail.com>
2020-04-26 14:23:31 +0100

    Add files via upload

---
+++ BatteryDischargeRigV10_sawtooth.ino
@@ -0,0 +1,423 @@
+//Battery discharge code written by Jack Joynson 22/07/16 for use with control panel.
+
+//TODO: IMPLEMENT NOBATTERY, NEWBATTERY AND ERROR LOGIC.
+        //CHECK DISCHARGING, DISCHARGED AND OFF LOGIC.
+        //IMPROVE ABOVE - START NEW DISCHARGE SHOULD RESET VALUES IF PREVIOUSLY DISCHARGED -NEW BATTERY?
+        //RESETING AMPHOURS ON GO.
+
+
+
+//CHANGE LOG:
+//USER          DATE        COMMENTS
+//Jack Joynson  22/07/2016  Create of new code to work with discharge board
+//Jack Joynson  27/07/2016  Created constants for each variable to allow for easier calibration
+//Jack Joynson  27/07/2016  Calibrated to watts up meter B
+//Jack Joynson  31/08/2016  Calibrated to Power supply
+//Jack Joynson  31/08/2016  Calibrated correctly to power supply, watts up meter and dvm. (Voltage averaging was removed too.)
+//Michael McCoy 01/09/2016  Calibrated using P1, P2 and N5
+//Tom Lawson    16/05/2017  Neatenimg and simplifying
+//Tom Lawson    18/05/2017  Calibrating current against DC Load from MTP Process Dev
+//Tom Lawson    02/06/2017  Calibration voltage against DMM
+//Tom Lawson    09/06/2017  Calibration voltage against DMM
+//Jack Joynson  23/07/2017  STARTED FROM V9 TO KEEP CALIBRATION VALUES - NEW LOGIC TO WORK WITH NEW DISCHARGE CONTROL PANEL
+//Jack Joynson  29/07/2017  Added triangle wave generation
+//#Include <PID_v1.h>
+const double currentCal_M = 0.141771066; //was 0.148063776;
+const double currentCal_C = 0.402; //was 0.2332; //was -1.4
+const double voltageCal_M = 0.030516973; //was 0.030658;
+const double voltageCal_C = -0.09327; //was 0.45313;
+const int voltageCountsTillFail = 125;
+const double minVoltage = 11.0;
+
+const double PWMStep = 0.01;
+
+
+
+// The arduinoStatus is sent as an integer and represents a bitwise status.
+  // 1 - Discharged // 2 - Discharging // 4 - Off // 8 - No Battery // 16 - New Battery // 32 - Error
+struct ArduinoStatus
+{
+  bool Discharged;
+  bool Discharging;
+  bool Off;
+  bool NoBattery;
+  bool NewBattery;
+  bool Error;
+};
+
+ArduinoStatus arduinoStatus = {false,false,true,false,false,false};
+//arduinoStatus.Discharged = false;
+//arduinoStatus.Discharging = false;
+//arduinoStatus.Off = true;
+//arduinoStatus.NoBattery = false;
+//arduinoStatus.NewBattery = false;
+//arduinoStatus.Error = false;
+
+const int VoltagePin = A0;
+const int CurrentPin = A1;
+const int PWMPin = 5;
+
+//These are used for averaging
+const int CurrentBufferSize = 100;
+const int VoltageBufferSize = 50;
+
+double MircoToHours = 1.0 / (1000.0 * 1000.0 * 60.0 * 60.0); //2.7777777777777777777777777777778e-10
+
+double PWMDuty = 0;
+double targetCurrent = 20.0;
+
+double voltage;
+double voltageBuffer[VoltageBufferSize];
+double voltageRunningTotal;
+bool voltageBufferFull = false;
+int8_t voltageBufferPosition = 0;
+
+double ampHours;
+unsigned long EnergyUsagePrevTime = 0;
+
+double current;
+double currentBuffer[CurrentBufferSize];
+double currentRunningTotal;
+bool currentBufferFull = false;
+int8_t currentBufferPosition = 0;
+
+int countsThisCycle = 0;
+int trianglePeriodCounts = 1000;
+double triangleAmplitude = 3;
+bool inTriangleMode = false;
+
+int8_t underVoltageCount = 0;
+
+int intervalDuration = 100;
+unsigned long tickTime = 0;
+
+double dischargeDuration = 0.0;
+unsigned long prevDisDur;
+
+int8_t diffDuration = 0;
+
+bool go = false;
+
+bool Running = false;
+
+
+void setup()
+{
+  Serial.begin(115200);
+
+  Serial.flush();
+
+//this is setting the PWM frequency on Timer 2
+  TCCR2B = TCCR2B & 0xF9; // was 0b11111000 | 0x01;
+
+  for (int i = 0; i < CurrentBufferSize; i++)
+  {
+    currentBuffer[i] = 0;
+  }
+
+  for (int i = 0; i < VoltageBufferSize; i++)
+  {
+    voltageBuffer[i] = 0;
+  }
+
+  SetTick();
+
+  pinMode(VoltagePin, INPUT);
+  pinMode(CurrentPin, INPUT);
+  pinMode(PWMPin, OUTPUT);
+}
+
+//this is the continously running main loop of the program
+void loop()
+{
+  SerialReceive();
+  ReadSensors();
+//if battery is discharged, reset everything
+  if (Discharged())
+  {
+    go = false;
+    Running = false;
+    arduinoStatus.Discharged = true;
+    arduinoStatus.Discharging = false;
+    arduinoStatus.Off = true;
+    digitalWrite(PWMPin, LOW);
+    PWMDuty = 0;
+  }
+  //if battery is not discharged
+  else {
+    if (go)
+    {
+      if (!Running)
+      {
+        Running = true;
+        dischargeDuration = 0.0;
+      }
+    }
+    else if (Running)
+    {
+      digitalWrite(PWMPin, LOW);
+      PWMDuty = 0;
+      Running = false;
+    }
+  }
+  CurrentControl();
+  if (TickElasped())
+  {
+    SerialTransmit();
+  }
+}
+
+//adjust pwm duty until current matches target
+void CurrentControl()
+{
+  if (Running)
+  {
+    double currentAim;
+    if(inTriangleMode)
+    {
+        countsThisCycle++;
+        if (countsThisCycle > trianglePeriodCounts) countsThisCycle = 0;
+        
+        int halfPeriod = trianglePeriodCounts/2;
+        if(countsThisCycle < halfPeriod)
+        {
+          currentAim = (2*triangleAmplitude*countsThisCycle/halfPeriod) + targetCurrent - triangleAmplitude;
+        }
+        else
+        {
+          currentAim = targetCurrent + triangleAmplitude - (2*triangleAmplitude*(countsThisCycle-halfPeriod)/halfPeriod);
+        }
+    }
+    else
+    {
+      currentAim = targetCurrent;
+    }
+    if (current > currentAim) PWMDown();     
+    else if (current < currentAim) PWMUp();
+    analogWrite(PWMPin, PWMDuty);
+  }
+}
+//fucntion tests for battery being discharged (battery voltage < 11 volts)
+bool Discharged()
+{
+  if (voltage < minVoltage)
+  {
+    if (underVoltageCount > voltageCountsTillFail)
+    {
+      return true;
+      arduinoStatus.Discharged = true;
+    }
+    else
+    {
+      underVoltageCount++;
+    }
+  }
+  else
+  {
+    underVoltageCount = 0;
+  }
+
+  return false;
+}
+
+void PWMUp()
+{
+  if (PWMDuty < 255)
+  {
+    PWMDuty += PWMStep;
+  }
+  else
+  {
+    PWMDuty = 255;
+  }
+}
+
+void PWMDown()
+{
+  if (PWMDuty > 0)
+  {
+    PWMDuty -= PWMStep;
+  }
+  else
+  {
+    PWMDuty = 0;
+  }
+}
+
+void ReadSensors()
+{
+  double tempCurrent = analogRead(CurrentPin);
+  unsigned long currTime = micros();
+  
+  CurrentAveraging((tempCurrent <= currentCal_C) ? 0.0 : ((tempCurrent * currentCal_M) + currentCal_C)); 
+  //this "?" operator is a ternery it means that
+  //currentAveraging input is 0 if(tempCurrent is <= currentCal_C) else ((tempCurrent * currentCal_M) + currentCal_C)
+
+   voltage = (analogRead(VoltagePin)*voltageCal_M) +voltageCal_C;
+
+
+  //AMPHOURS IS CURRENT * CHANGE IN TIME.
+
+  double EnergyAddition = current * ((currTime - EnergyUsagePrevTime) * MircoToHours);
+  if (EnergyAddition > 0) //LOGIC ADDED TO CHECK THAT THE AMPHOURS ONLY ADDS AND DOESNT GET RESET. IF ARDUINO RESETS AND CURRTIME = 0 THEN THE LAST VALUE WOULD HAVE RESET THE TOTAL.
+  {
+    ampHours += EnergyAddition;
+  }
+  EnergyUsagePrevTime = currTime;
+}
+
+//this is system time
+void SetTick()
+{
+  tickTime = millis() + intervalDuration;
+}
+
+bool TickElasped()
+{
+  if (tickTime < millis())
+  {
+    SetTick();
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+void CurrentAveraging(double input)
+{
+  double removedValue = currentBuffer[currentBufferPosition];
+  
+  currentBuffer[currentBufferPosition] = input;
+  currentRunningTotal += input;
+
+  if (currentBufferFull)
+  {
+    currentRunningTotal -= removedValue;
+    if (currentBufferPosition == (CurrentBufferSize - 1))
+    {
+      currentBufferPosition = 0;
+    }
+    else
+    {
+      currentBufferPosition++;
+    }
+
+    current = currentRunningTotal / CurrentBufferSize;
+    return;
+  }
+  else if (currentBufferPosition == (CurrentBufferSize - 1))
+  {
+    currentBufferFull = true;
+    currentBufferPosition = 0;
+
+    return;
+  }
+  else
+  {
+    currentBufferPosition++;
+  }
+}
+
+void SerialReceive()
+{
+  if (Serial.available() > 0)
+  {
+    char firstVal = Serial.read();
+    if (firstVal == 'g' || firstVal == 'G')
+    {
+      go = true;
+      arduinoStatus.Discharged = false;
+      arduinoStatus.Discharging = true;
+      arduinoStatus.Off = false;
+      inTriangleMode = false;
+      
+      //RESET VALUES
+      ampHours = 0;
+      underVoltageCount = 0;
+      
+      digitalWrite(13, HIGH);
+    }
+    else if(firstVal == 's' || firstVal == 'S')
+    {
+      go = true;
+      arduinoStatus.Discharged = false;
+      arduinoStatus.Discharging = true;
+      arduinoStatus.Off = false;
+      inTriangleMode = true;
+      
+      //RESET VALUES
+      ampHours = 0;
+      underVoltageCount = 0;
+      
+      digitalWrite(13, HIGH);
+    }
+    else if (firstVal == 'o' || firstVal == 'O')
+    {
+      go = false;
+      arduinoStatus.Off = true;
+      arduinoStatus.Discharging = false;
+    }
+    else if (firstVal == 'c' || firstVal == 'C')
+    {
+      int temp = Serial.parseInt();
+      if (temp > 0)
+      {
+        targetCurrent = temp;
+      }
+    }
+    else if(firstVal == 'p' || firstVal == 'P')
+    {
+      int temp = Serial.parseInt();
+      if(temp > 0)
+      {
+        trianglePeriodCounts = temp;
+      }
+    }
+    else if(firstVal == 'a' || firstVal == 'A')
+    {
+      double temp = Serial.parseFloat();
+      if(temp> 0)
+      {
+         triangleAmplitude = temp; 
+      }
+    }
+  }
+}
+
+void SerialTransmit()
+{
+  if (Running)
+  {
+    unsigned long curMillis = millis();
+    unsigned long dif = curMillis - prevDisDur;
+    if(dif > 0) dischargeDuration += dif;
+    prevDisDur = curMillis;
+  }
+  Serial.print(GetArduinoStatusValue()); //Status
+  Serial.print(",");
+  Serial.print(current); 
+  Serial.print(",");
+  Serial.print(ampHours);
+  Serial.print(",");
+  Serial.print(voltage);
+  Serial.print(",");
+  Serial.print(targetCurrent);
+  Serial.print(",");
+  Serial.print(PWMDuty*100/255);
+  Serial.print(",");
+  Serial.println(dischargeDuration);
+}
+
+int GetArduinoStatusValue()
+{
+  int toReturn = 0;
+  if(arduinoStatus.Discharged)toReturn += 1;
+  if(arduinoStatus.Discharging)toReturn += 2;
+  if(arduinoStatus.Off)toReturn += 4;
+  if(arduinoStatus.NoBattery)toReturn += 8;
+  if(arduinoStatus.NewBattery)toReturn += 16;
+  if(arduinoStatus.Error)toReturn += 32;  
+  return toReturn;
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,449 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+using Microsoft.Office.Core;
+using Forms = System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace BatteryLogPlotter
+{
+   /// <summary>
+   /// Battery plotting tool by Jack Joynson.
+   /// Scans network files then produces CSVs of final results
+   /// </summary>
+   public partial class MainWindow : Window
+   {
+      //Global variables
+      string batteryLogsPath = @"\\UKNML7112\GREENPOWER\DISCHARGE RIG\Discharge Logs";
+      string saveReportsPath = @"\\UKNML7112\GREENPOWER\DISCHARGE RIG\Reports";
+      List<string> selectableFiles = new List<string>();
+      List<string> selectableCodes = new List<string>();
+      private bool showErrors = true;
+
+      int minimumFileLines = 20;
+
+      private enum cellColumns
+      {
+         status = 'A',
+         current = 'B',
+         amphours = 'C',
+         voltage = 'D',
+         aim = 'E',
+         PWM = 'F',
+         time = 'G'
+      }
+
+      private enum splitColumns
+      {
+         status,
+         current,
+         amphours,
+         voltage,
+         aim,
+         PWM,
+         time
+      }
+
+      private string[] columnNames = {"Status","Current","Amphours","Voltage","CurrentAim","PWM","Time"};
+      private bool[] saveColumn = { false, false, true, true, true, false, true };
+
+
+      private int numberColumns = 7;
+
+      public MainWindow()
+      {
+         InitializeComponent();
+
+         setDirectoryBox();
+
+
+         populateDischargeFilesListBox();
+         populateBatteryCodesListBox();
+      }
+
+      #region ************************************************************ initialisations methods **************************************************
+
+      private void setDirectoryBox()
+      {
+         txtDirectory.Text = batteryLogsPath;
+      }
+
+      #endregion
+
+      #region ************************************************************ button events ************************************************************
+
+      private void btnGenerateSingleReport_Click(object sender, RoutedEventArgs e)
+      {
+         showSimpleDialog("This has not been completed yet");
+         int selectedIndex = lstDischargeFiles.SelectedIndex;
+         if (selectedIndex >= 0)
+         {
+            string name = (string)lstDischargeFiles.SelectedValue;
+            createExcel(selectableFiles[selectedIndex], name);
+         }
+      }
+
+      private void btnGenerateBatteryReport_Click(object sender, RoutedEventArgs e)
+      {
+         int selectedIndex = lstBatteryCodes.SelectedIndex;
+         if (selectedIndex >= 0)
+         {
+            //showSimpleDialog(selectableCodes[selectedIndex]);
+            generateBatteryReport(selectableCodes[selectedIndex]);
+         }
+      }
+
+      private void btnBrowse_Click(object sender, RoutedEventArgs e)
+      {
+         Forms.FolderBrowserDialog folderBrowser = new Forms.FolderBrowserDialog();
+         folderBrowser.Description = "Select the folder containing the battery discharge logs";
+         folderBrowser.SelectedPath = batteryLogsPath;
+         if (folderBrowser.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+         {
+            if (Directory.Exists(folderBrowser.SelectedPath))
+            {
+               batteryLogsPath = folderBrowser.SelectedPath;
+               setDirectoryBox();
+               populateDischargeFilesListBox();
+               populateBatteryCodesListBox();
+            }
+            else
+            {
+               showSimpleDialog("Could not find the selected directory", "Error", Forms.MessageBoxIcon.Error);
+            }
+
+         }
+      }
+
+      #endregion
+
+      #region ************************************************************ General method ***********************************************************
+
+      private void showSimpleDialog(string message, string title = "Message", Forms.MessageBoxIcon icon = Forms.MessageBoxIcon.None)
+      {
+         Forms.MessageBox.Show(message, title, Forms.MessageBoxButtons.OK, icon);
+      }
+
+      #endregion
+
+      #region ************************************************************ Misc methods *************************************************************
+
+      private void populateDischargeFilesListBox()
+      {
+         if (Directory.Exists(batteryLogsPath))
+         {
+            lstDischargeFiles.Items.Clear();
+            selectableFiles.Clear();
+            int length = batteryLogsPath.Length + 1;
+            try
+            {
+               string[] files = Directory.GetFiles(batteryLogsPath);
+               foreach (string file in files)
+               {
+                  if (file.Length > (4 + length) && file.Contains(".csv"))
+                  {
+                     string toAdd = file.Substring(length, file.IndexOf(".csv") - length);
+                     lstDischargeFiles.Items.Add(toAdd);
+                     selectableFiles.Add(file);
+                  }
+               }
+
+
+            }
+            catch (Exception err)
+            {
+               showSimpleDialog("An error occured fetching the files in: " + batteryLogsPath + ". Error: " + err, "File error", Forms.MessageBoxIcon.Error);
+            }
+         }
+      }
+
+      private void populateBatteryCodesListBox()
+      {
+         if (Directory.Exists(batteryLogsPath))
+         {
+            lstBatteryCodes.Items.Clear();
+            selectableCodes.Clear();
+            int length = batteryLogsPath.Length + 1;
+            try
+            {
+               string[] files = Directory.GetFiles(batteryLogsPath);
+               foreach (string file in files)
+               {
+                  string postDir = file.Substring(length);
+                  if (postDir.Contains(' ') && postDir.Length > 4 && postDir.EndsWith(".csv"))
+                  {
+                     int spaceLoc = postDir.IndexOf(' ');
+                     string toAdd = postDir.Substring(0, spaceLoc).ToUpper();
+                     bool passed = true;
+                     foreach (string code in selectableCodes)
+                     {
+                        if (toAdd == code)
+                        {
+                           passed = false;
+                        }
+                     }
+                     if (passed)
+                     {
+                        selectableCodes.Add(toAdd);
+                     }
+                  }
+               }
+
+               selectableCodes.Sort();
+               foreach (string item in selectableCodes)
+               {
+                  lstBatteryCodes.Items.Add(item);
+               }
+            }
+            catch (Exception err)
+            {
+               showSimpleDialog("An error occured fetching the files in: " + batteryLogsPath + ". Error: " + err, "File error", Forms.MessageBoxIcon.Error);
+            }
+         }
+      }
+      #endregion
+
+
+      private void allButton_Click(object sender, RoutedEventArgs e)
+      {
+         Process.Start(saveReportsPath);
+         showErrors = false;
+         for (int i = 0; i < selectableCodes.Count; i++) {
+            generateBatteryReport(selectableCodes[i]);
+         }
+         showErrors = true;
+      }
+
+
+      private void generateBatteryReport(string code)
+      {
+         code += " ";
+         string[] reports = new string[numberColumns];
+         int pathLength = batteryLogsPath.Length + 1;
+         string[] networkFiles = Directory.GetFiles(batteryLogsPath);
+         foreach (string file in networkFiles)
+         {
+            string postDir = file.Substring(pathLength);
+            if (postDir.StartsWith(code))
+            {
+               string lastData = getFileData(file);
+               if (lastData.Length > 5)
+               {
+                  setReports(lastData, ref reports);
+               }
+               else
+               {
+                  lastData = getFileData(file, 5);
+                  if(lastData.Length < 5)
+                  {
+                     if (showErrors)
+                     {
+                        showSimpleDialog("File lines all seem too small: " + file);
+                     }
+                  }
+                  setReports(lastData, ref reports);
+               }
+            }
+         }
+
+         saveReports(reports, code);
+
+         if (showErrors)
+         {
+            Process.Start(saveReportsPath);
+         }
+      }
+
+      private void setReports(string data, ref string[] reports)
+      {
+         string[] splits = data.Split(',');
+         if (splits.Length == numberColumns) {
+            for (int i = 0; i < numberColumns; i++)
+            {
+               reports[i] += splits[i] +",";
+            }
+         }
+         else
+         {
+            if (showErrors)
+            {
+               showSimpleDialog("Invalid number of deliminators: " + splits.Length);
+            }
+         }
+
+
+      }
+
+      private string getFileData(string file, int numberFromEnd = 2)
+      {
+         try
+         {
+            string[] lines = File.ReadAllLines(file);
+            int length = lines.Length - numberFromEnd;
+            if (length >= minimumFileLines) {
+               return lines[length];
+            }
+            else
+            {
+               if (showErrors)
+               {
+                  showSimpleDialog("Not enough lines in: " + file + ". Only found: " + length);
+               }
+            }
+         }
+         catch(Exception err)
+         {
+            showSimpleDialog("Error accessing file: " + file + ". Error code: " + err);
+         }
+         return "";
+      }
+
+      private void saveReports(string[] reports, string code)
+      {
+         for (int i = 0; i < numberColumns; i++)
+         {
+            if (saveColumn[i])
+            {
+               try
+               {
+                  File.WriteAllText(saveReportsPath + @"\" + code + columnNames[i]+ " report.csv", reports[i]);
+               }
+               catch (Exception err)
+               {
+                  showSimpleDialog("An error occured creating the report: " + err);
+               }
+            }
+         }
+      }
+
+
+
+
+
+
+
+      private void createExcel(string path, string name)
+      {
+         object oMissing = System.Reflection.Missing.Value;
+
+         Excel.Application xlApp = new Excel.Application();
+         Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(path);
+         Excel.Worksheet xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+         xlApp.Visible = true;
+
+         //createExcelChart(xlWorkSheet, oMissing, "A"); //Status graph
+
+         cellColumns chartType = cellColumns.current;
+         createExcelChart(xlWorkSheet, oMissing, chartType); //Current
+
+
+         tryCreateDirectory(saveReportsPath);
+         string saveName = getSavePath(saveReportsPath, "Excel save name", ".xls", name);
+         showSimpleDialog(saveName);
+         if (saveName != "" && saveName != null && saveName != name)
+         {
+            xlWorkBook.SaveAs(saveName, Excel.XlFileFormat.xlWorkbookNormal,
+                oMissing, oMissing, oMissing, oMissing, Excel.XlSaveAsAccessMode.xlExclusive,
+                oMissing, oMissing, oMissing, oMissing, oMissing);
+         }
+
+         xlWorkBook.Close(true, oMissing, oMissing);
+         xlApp.Quit();
+
+         releaseObject(xlWorkSheet);
+         releaseObject(xlWorkBook);
+         releaseObject(xlApp);
+      }
+
+      private void releaseObject(object obj)
+      {
+         try
+         {
+            if (obj != null)
+            {
+               System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+               obj = null;
+            }
+         }
+         catch (Exception err)
+         {
+            obj = null;
+            MessageBox.Show("Exception Occured while releasing object " + err.ToString());
+         }
+         finally
+         {
+            GC.Collect();
+         }
+      }
+
+      private string getSavePath(string iniDirectory, string message, string ext, string name = "")
+      {
+         Forms.SaveFileDialog dialog = new Forms.SaveFileDialog();
+         dialog.FileName = name;
+         dialog.DefaultExt = ext;
+         dialog.InitialDirectory = iniDirectory;
+         dialog.Title = message;
+
+         dialog.ShowDialog();
+         string savePath = dialog.FileName;
+         if (savePath == null || savePath.Length < 5)
+         {
+            savePath = getSavePath(iniDirectory, message, ext, name);
+         }
+         return savePath;
+      }
+
+      private void tryCreateDirectory(string path)
+      {
+         if (!Directory.Exists(path))
+         {
+            try
+            {
+               Directory.CreateDirectory(path);
+            }
+            catch (Exception err)
+            {
+               showSimpleDialog("An error occured creating the save reports directory (" + path + ") Error: " + err, "Directory error", Forms.MessageBoxIcon.Error);
+               Environment.Exit(1);
+            }
+         }
+      }
+
+
+      private void createExcelChart(Excel.Worksheet xlWorkSheet, object oMissing, cellColumns type)
+      {
+         string rangeEndLetter = "" + (char)type;
+
+         Excel.Range chartRange;
+
+         Excel.ChartObjects xlCharts = (Excel.ChartObjects)xlWorkSheet.ChartObjects(Type.Missing);
+         Excel.ChartObject myChart = (Excel.ChartObject)xlCharts.Add(10, 80, 300, 250);
+         Excel.Chart chartPage = myChart.Chart;
+
+         int rowCounts = xlWorkSheet.Rows.Count;
+         string rangeEndConverted = "" + rangeEndLetter;
+         string rangeEnd = rangeEndConverted + rowCounts;
+
+         chartRange = xlWorkSheet.get_Range("A1", rangeEnd);
+         chartPage.SetSourceData(chartRange, oMissing);
+         chartPage.ChartType = Excel.XlChartType.xlLineMarkers;
+      }
+
+   }
+}
